4c92a7c53d8bc1ebcc81d494624c8a1067bc67d2
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-06-16 22:16:20 +0530

    feat: update contract address

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { Box, Flex, Button, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
-import { useContract, useProvider, useSigner } from "wagmi";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
 import WLMint from "../utils/WhitelistMint.json";
 import { Biconomy } from "@biconomy/mexa";
 import { ethers } from "ethers";
+import Head from "next/head";
 
 declare const window: any;
 
 const Home: NextPage = () => {
 	const [loading, setLoading] = useState(false);
 
-	// const { data: signer } = useSigner();
+	const { data } = useAccount();
 	let provider: ethers.providers.Provider;
 
 	let biconomy: any;
@@ -19,42 +20,52 @@ const Home: NextPage = () => {
 	let contract: any;
 
 	useEffect(() => {
-		if (window.ethereum !== "undefined") {
+		if (window.ethereum != "undefined") {
 			provider = new ethers.providers.Web3Provider(window.ethereum);
 			biconomy = new Biconomy(provider, {
-				apiKey: "a86UV8aKm.b5d04fac-a2f3-442e-9dd8-5bb58f1614c2",
+				apiKey: process.env.NEXT_PUBLIC_BICO_API as string,
 				debug: false,
 			});
 			biconomyProvider = new ethers.providers.Web3Provider(biconomy);
+		} else {
+			alert("Ethereum enviornment not found");
 		}
 	});
 
 	const mint = async () => {
-		setLoading(true);
-		const signer = biconomyProvider.getSigner();
-		contract = new ethers.Contract(
-			"0xd3Ca0B0eA48A7a9943C7b0A44ECE57CedDC58D1b",
-			WLMint.abi,
-			signer
-		);
-		try {
-			let txn = await contract.mint({
-				gasPrice: 25,
-				gasLimit: 9000000,
-			});
-			console.log(txn);
-			setLoading(false);
-			alert(`Txn completed with txn hash: ${txn.hash}`);
-		} catch (e) {
-			console.log(e);
-			setLoading(false);
+		if (data?.address) {
+			setLoading(true);
+			const signer = biconomyProvider.getSigner();
+			contract = new ethers.Contract(
+				"0xd6C57F0c8fEbc752a7b202229E74A0cEac676d87",
+				WLMint.abi,
+				signer
+			);
+			try {
+				let txn = await contract.mint({
+					gasPrice: 25,
+					gasLimit: 9000000,
+				});
+				console.log(txn);
+				setLoading(false);
+				alert(`Txn completed with txn hash: ${txn.hash}`);
+			} catch (e) {
+				console.log(e);
+				alert("An error occured, check console");
+				setLoading(false);
+			}
+		} else {
+			alert("Connect wallet first");
 		}
 	};
 
 	return (
 		<Box p={6}>
+			<Head>
+				<title>NFT Drop $DK</title>
+			</Head>
 			<Flex
-				justifyItems={"center"}
+				justifyContent={"center"}
 				alignItems="center"
 				direction={"column"}
 				background={"whiteAlpha.100"}
@@ -63,7 +74,7 @@ const Home: NextPage = () => {
 				minHeight={"20rem"}
 				minWidth={"25rem"}
 			>
-				<Text m={4}>
+				<Text mb={4}>
 					Click the Mint button below to mint one of a kind $DK ERC721
 					token for free (not even gas!)
 				</Text>
